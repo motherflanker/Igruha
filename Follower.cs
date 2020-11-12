e6f98bd397401618e897d531c52ef283bca035e9
motherflanker <74090094+motherflanker@users.noreply.github.com>
2020-11-12 16:43:01 +0300

    Add files via upload

--- Follower.cs
+++ Follower.cs
@@ -2,21 +2,58 @@
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
 public class Follower : MonoBehaviour
 {
 
-    public Transform target;
-    public float smoothTime = 0.3f;
-    private Vector3 velocity = Vector3.zero;
+    private Transform player;
+    public float dumping = 1.5f;
+    public Vector2 offset = new Vector2(2f, 1f);
+    public bool isLeft;
+    private int lastX;
 
    
 
-    // Update is called once per frame
+    void Start()
+    {
+        offset = new Vector2(Mathf.Abs(offset.x), offset.y);
+        FindPlayer(isLeft);
+    }
+
+    public void FindPlayer(bool playerIsLeft)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        lastX = Mathf.RoundToInt(player.position.x);
+        if (playerIsLeft)
+        {
+            transform.position = new Vector3(player.position.x - offset.x, player.position.y - offset.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
+        }
+    }
+
     void Update()
     {
-        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -20));
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime, 10f);
+        if (player)
+        {
+            int currentX = Mathf.RoundToInt(player.position.x);
+            if (currentX > lastX) isLeft = false;
+            else if (currentX < lastX) isLeft = true;
+            Vector3 target;
+            if (isLeft)
+            {
+                target = new Vector3(player.position.x - offset.x, player.position.y + offset.y, transform.position.z);
+            }
+            else
+            {
+                target = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
+            }
+            Vector3 currentPos = Vector3.Lerp(transform.position, target, dumping * Time.deltaTime);
+            transform.position = currentPos;
+        }
     }
 }
